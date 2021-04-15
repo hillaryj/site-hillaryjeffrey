deb7e1901a3d56fa44e8b0977aa04337cea1b2ea
Hillary Jeffrey <hillary.jeffrey@cisa.dhs.gov>
2021-04-15 09:06:06 -0400

    Remove Disqus

--- src/templates/page.js
+++ src/templates/page.js
@@ -6,7 +6,6 @@ import Content from '../components/Content'
 import Wrapper from '../components/Wrapper'
 import Hero from '../components/Hero'
 import SEO from '../components/SEO'
-import Disqus from '../components/Disqus'
 
 const Page = props => {
   const page = props.data.page
@@ -30,12 +29,6 @@ const Page = props => {
           <Content content={page.body} date={page.frontmatter.date} />
         </article>
       </Wrapper>
-
-      {page.frontmatter.disqus && (
-        <Wrapper as="aside">
-          <Disqus slug={page.frontmatter.slug} title={page.frontmatter.title} />
-        </Wrapper>
-      )}
     </Layout>
   )
 }
@@ -51,7 +44,6 @@ export const pageQuery = graphql`
         title
         date(formatString: "MMMM DD, YYYY")
         slug
-        disqus
         cover {
           publicURL
         }
