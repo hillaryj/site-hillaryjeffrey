e30062edd8fa1deee65b86090c644250f06d4033
Hillary Jeffrey <hillary.jeffrey@cisa.dhs.gov>
2021-04-15 09:06:15 -0400

    Initial site config and about

--- content/pages/about/about.md
+++ content/pages/about/about.md
@@ -1,8 +1,7 @@
 ---
 title: "About gatsby-starter-morning-dew"
-slug: about-gatsby-starter-morning-dew
+slug: about
 cover: ./gatsby-cover.png
-disqus: true
 ---
 
 [![Build Status](https://travis-ci.org/maxpou/gatsby-starter-morning-dew.svg?branch=master)](https://travis-ci.org/maxpou/gatsby-starter-morning-dew) [![Netlify Status](https://api.netlify.com/api/v1/badges/d68027bd-19b5-44fd-a2a3-d1f05fc8201e/deploy-status)](https://app.netlify.com/sites/gatsby-starter-morning-dew/deploys) [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier) [![Maintainability](https://api.codeclimate.com/v1/badges/e09f1ac1a5cdb2987a6f/maintainability)](https://codeclimate.com/github/maxpou/gatsby-starter-morning-dew/maintainability) [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
@@ -28,7 +27,7 @@ A gatsby-starter-morning-dew is a Gatsby theme/starter to build lightning-fast w
 - 🌜 Dark mode
 - ✨ PWA ready
   - ✈️ Offline support
-  - 📃 Manifest support 
+  - 📃 Manifest support
 - [🔧 Fully configurable (see data/siteConfig.js)](./data/siteConfig.js)
 - 💬 Disqus support
 - 💅 css-in-js (with [styled-components](https://www.styled-components.com))
--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,42 +1,39 @@
 module.exports = {
-  siteTitle: 'gatsby-starter-morning-dew',
-  siteDescription: 'A Gatsby theme/starter to build lightning-fast websites',
-  authorName: 'Maxence Poutord',
-  twitterUsername: '_maxpou',
-  authorAvatar: 'avatar.jpeg', // file in content/images
+  siteTitle: 'Hillary on the web',
+  siteDescription: 'Hello. I am not a dog, I swear.',
+  authorName: 'Hillary Jeffrey',
+  twitterUsername: 'DeputyHuman',
+  authorAvatar: 'belle-ball.jpg', // file in content/images
   defaultLang: 'en', // show flag if lang is not default. Leave empty to enable flags in post lists
-  authorDescription: `
-  For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
-  On his day to day job, he is working as a senior front-end engineer at VSware. He is also an occasional tech speaker and a mentor.
-  As a digital nomad, he is living where the WiFi and sun are 😎 <br>
-  Do you want to know more? <a href="https://www.maxpou.fr/about" rel="noopener" target="_blank">Visit my website!</a>
-  `,
-  siteUrl: 'https://maxpou.github.io/',
-  disqusSiteUrl: 'https://www.maxpou.fr/',
+  authorDescription: `Hillary is human, or at least deputized to be one.`,
+  siteUrl: 'https://www.hillaryjeffrey.com/',
   // Prefixes all links. For cases when deployed to maxpou.fr/gatsby-starter-morning-dew/
-  pathPrefix: '/gatsby-starter-morning-dew', // Note: it must *not* have a trailing slash.
-  siteCover: 'cover-baymax.jpeg', // file in content/images
+  // pathPrefix: '/hillaryjeffrey', // Note: it must *not* have a trailing slash.
+  siteCover: 'concert-lighting.png', // file in content/images
   background_color: '#ffffff',
-  theme_color: '#222222',
+  theme_color: '#463591',
   display: 'standalone',
-  icon: 'content/images/baymax.png',
+  icon: 'content/images/belle-ball.jpg',
   postsPerPage: 6,
-  disqusShortname: 'maxpou',
-  headerTitle: 'gatsby-starter-morning-dew',
-  headerLinksIcon: 'baymax.png', //  (leave empty to disable: '')
+  headerTitle: 'Hillary on the web',
+  headerLinksIcon: 'belle-ball.jpg', //  (leave empty to disable: '')
   headerLinks: [
     {
-      label: 'Blog',
+      label: 'Home',
       url: '/',
     },
     {
       label: 'About',
-      url: '/about-gatsby-starter-morning-dew',
+      url: '/about',
     },
     {
-      label: 'Installation',
-      url: '/how-to-install',
+      label: 'Recipes',
+      url: '/recipes',
     },
+    // {
+    //   label: 'Github',
+    //   url: 'https://github.com/hillaryj',
+    // },
   ],
   // Footer information (ex: Github, Netlify...)
   websiteHost: {
@@ -48,16 +45,16 @@ module.exports = {
       sectionName: 'Explore',
       links: [
         {
-          label: 'Blog',
+          label: 'Home',
           url: '/',
         },
         {
           label: 'About',
-          url: '/about-gatsby-starter-morning-dew',
+          url: '/about',
         },
         {
-          label: 'Installation',
-          url: '/how-to-install',
+          label: 'Recipes',
+          url: '/recipes',
         },
       ],
     },
@@ -65,18 +62,18 @@ module.exports = {
       sectionName: 'Follow the author',
       links: [
         {
-          label: 'GitHub',
-          url: 'https://github.com/maxpou/gatsby-starter-morning-dew',
+          label: 'Evil Hillary',
+          url: 'https://www.evilhillary.com',
           rel: 'external',
         },
         {
-          label: 'Website',
-          url: 'https://www.maxpou.fr',
+          label: 'GitHub',
+          url: 'https://github.com/hillaryj',
           rel: 'external',
         },
         {
           label: 'Twitter',
-          url: 'https://twitter.com/_maxpou',
+          url: 'https://twitter.com/DeputyHuman',
           rel: 'external',
         },
       ],
